8f8c335410a491b8fe6534fce6125954dd41dbec
SofiaKubo <skuboreva@gmail.com>
2025-11-14 14:07:27 +0100

    feat(sorting): implement resetting of inactive columns and restore sorting state

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -12,8 +12,23 @@ export function initSorting(columns) {
       order = action.dataset.value; // Направление заберём прямо из датасета для точности
 
       // @todo: #3.2 — сбросить сортировки остальных колонок
+      columns.forEach((column) => {
+        // Перебираем элементы (в columns у нас массив кнопок)
+        if (column.dataset.field !== action.dataset.field) {
+          // Если это не та кнопка, что нажал пользователь
+          column.dataset.value = "none"; // тогда сбрасываем её в начальное состояние
+        }
+      });
     } else {
       // @todo: #3.3 — получить выбранный режим сортировки
+      columns.forEach((column) => {
+        // Перебираем все наши кнопки сортировки
+        if (column.dataset.value !== "none") {
+          // Ищем ту, что находится не в начальном состоянии (предполагаем, что одна)
+          field = column.dataset.field; // Сохраняем в переменных поле
+          order = column.dataset.value; // и направление сортировки
+        }
+      });
     }
 
     return sortCollection(data, field, order);
